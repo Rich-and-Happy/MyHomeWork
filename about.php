beeafd99935b9d05b0fd7676124c4ac78c1ee9ef
Rich-and-Happy <r.h9@yandex.ru>
2016-11-19 23:12:43 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+$name_user = 'Timur ';
+$age_user = '30 ';
+$mail_user = 'ya@ya.ru ';
+$city_user = 'Saint-Petersburg ';
+$about_user = 'noob ';
+
+echo $name_user;
+echo $age_user;
+echo $mail_user;
+echo $city_user;
+PHP_EOL;
+echo $about_user;
+?>
