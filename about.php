3073db0e599b96b7b39f903ee5a91c2ea05c972f
Rich-and-Happy <r.h9@yandex.ru>
2016-11-21 11:51:01 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,14 +1,23 @@
+
 <?php
 $name_user = 'Timur ';
 $age_user = '30 ';
 $mail_user = 'ya@ya.ru ';
 $city_user = 'Saint-Petersburg ';
 $about_user = 'noob ';
-
-echo $name_user;
-echo $age_user;
-echo $mail_user;
-echo $city_user;
-PHP_EOL;
-echo $about_user;
 ?>
+
+<html>
+    <head>
+        <title>Home Work</title>
+    </head>
+    <body>
+      <h1> About User </h1>
+<p><?= $name_user ?></p>
+<p><?= $age_user ?></p>
+<p><?= $mail_user ?></p>
+<p><?= $city_user ?></p>
+<p><?= $about_user ?></p>
+
+    </body>
+</html>
